39c5ad32227b805a109991a1519190af1fb33be5
AdhilshaA <111010444+AdhilshaA@users.noreply.github.com>
2022-08-11 15:43:47 +0530

    Update pRNG1.py

--- pRNG1.py
+++ pRNG1.py
@@ -1,6 +1,17 @@
+import matplotlib.pyplot as plt
+
+x-axis=list(i for i in range(1,201))
 RNs =[0.1]
 seed = 0.1
 c = 3.5
 for i in range(1,201):
     RNs[i] = RNs[i-1] * c * (1 - RNs[i-1])
 
+plt.plot(x-axis,RNs)
+
+plt.xlabel('index of random number')
+plt.ylabel('Random number')
+
+plt.title('pRNG')
+
+plt.show()
